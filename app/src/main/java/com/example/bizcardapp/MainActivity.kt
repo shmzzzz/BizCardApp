620a055f82c589b6ebd6dce589480a62551bdb26
shmzzzz <slt0623@gmail.com>
2024-04-25 15:04:19 +0900

    feat: プロフィール画像を表示できるようにする

--- app/src/main/java/com/example/bizcardapp/MainActivity.kt
+++ app/src/main/java/com/example/bizcardapp/MainActivity.kt
@@ -4,23 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.bizcardapp.ui.theme.BizCardAppTheme
@@ -55,6 +60,23 @@ fun CreateBizCard() {
             shape = RoundedCornerShape(corner = CornerSize(15.dp)),
             colors = CardDefaults.cardColors(containerColor = Color.White),
         ) {
+
+            Surface(
+                modifier = Modifier
+                    .size(150.dp)
+                    .padding(5.dp),
+                shape = CircleShape,
+                border = BorderStroke(0.5.dp, Color.LightGray),
+                tonalElevation = 4.dp,
+                color = MaterialTheme.colorScheme.background.copy(alpha = 0.5f),
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.profile_image),
+                    contentDescription = "profile image",
+                    modifier = Modifier.size(135.dp),
+                    contentScale = ContentScale.Crop,
+                )
+            }
 
         }
 
